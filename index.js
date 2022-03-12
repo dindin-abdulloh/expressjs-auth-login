97e601245772c305b0d2809f86b5e0541723d0f5
Dindin Abduloh <dindin.abduloh11@gmail.com>
2022-03-12 15:45:25 +0700

    done

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import db from "./config/Database.js";
 import router from "./routes/index.js";
+import cookieParser from "cookie-parser";
 import dotenv from 'dotenv'
+import cors from "cors"
+
 // import Users from "./models/UserModel.js";
 const app = express()
 dotenv.config()
@@ -14,6 +17,9 @@ try {
     console.log(error);
 }
 
+
+app.use(cors({credentials:true, origin:'http://localhost:3000'}))
+app.use(cookieParser())
 app.use(express.json())
 app.use(router)
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { getUser, registerUser, loginUser } from "../controllers/Users.js";
+import { getUser, registerUser, loginUser, logoutUser } from "../controllers/Users.js";
+import { verifyToken } from "../middleware/VerifyToken.js";
+import { refreshToken } from "../controllers/RefreshToken.js";
 
 const router = express.Router()
 
-router.get('/users', getUser)
+router.get('/users', verifyToken, getUser)
 router.post('/users', registerUser)
 router.post('/login', loginUser)
+router.get('/token', refreshToken)
+router.delete('/logout', logoutUser)
 
 export default router
